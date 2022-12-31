4a0ef27c1f0189a7d2499a79d25a9847828c2443
Ahmed El-Brawany <91606645+ahmedelbrawany@users.noreply.github.com>
2022-12-31 17:16:16 +0200

    Queue at the School solution

---
+++ Queue at the School/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int childern_num;
+    int seconds_countdown;
+    string boys_girls;
+    cin>>childern_num>>seconds_countdown>>boys_girls;
+
+    char temp;
+    childern_num--;
+    for(;seconds_countdown!=0; seconds_countdown--){
+        for(int i=0; i<childern_num;i++){
+            if(boys_girls[i]=='B'&& boys_girls[i+1]=='G'){
+                boys_girls[i]='G';
+                boys_girls[i+1]='B';
+                i++;
+            }
+        }
+    }
+    cout<<boys_girls<<endl;
+
+    return 0;
+}
